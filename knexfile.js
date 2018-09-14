2cfd6721f9f2a1e9bfcb9ee1df154da12eea8756
joseph walter <joeywalter@josephs-MacBook-Pro.local>
2018-09-14 11:34:09 -0600

    set up knex and postgresql

---
+++ knexfile.js
@@ -0,0 +1,16 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: 'postgresql://localhost/my_g95_database'
+  },
+// deleted staging and left client and connection for dev and production.
+// changed client to pg because I am using postgresql
+// connected to development to the database I created with connection :'postgresql://localhost/my_g95_database'
+  production: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL
+  }
+};
